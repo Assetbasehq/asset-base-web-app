42a0762af8a358d9b65f036407d5392758983448
ObodoakorDavid <obodoakordavid@outlook.com>
2025-09-28 15:54:38 +0100

    fix

--- src/pages/dashboard/wallet/deposit/_layouts/deposit-layout.tsx
+++ src/pages/dashboard/wallet/deposit/_layouts/deposit-layout.tsx
@@ -1,10 +1,10 @@
-import WalletBreadCrumb from "../../_components/wallet-bread-crumb";
 import { Outlet } from "react-router";
+import DepositBreadCrumb from "../_components/deposit-bread-crumb";
 
 export default function DepositLayout() {
   return (
     <div>
-      <WalletBreadCrumb />
+      <DepositBreadCrumb />
 
       <div className="flex flex-col gap-8 text-start w-full max-w-md mx-auto">
         <Outlet />
